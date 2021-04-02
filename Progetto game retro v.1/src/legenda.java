550034b9bf7d2e847552117a82dd81ef97a0c00b
Beocamik <63676260+Beocamik@users.noreply.github.com>
2021-04-02 15:08:09 +0200

    Add files via upload

---
+++ Progetto game retro v.1/src/legenda.java
@@ -0,0 +1,10 @@
+
+public class legenda {
+
+	public legenda(String legenda [] []) {
+		// TODO Auto-generated constructor stub
+		legenda [][]=new String [6][1];
+		
+	}
+
+}
---
+++ Progetto game retro v.1/src/map.java
@@ -0,0 +1,19 @@
+
+public class map {
+	
+	private int v [][];
+	private char c = '@';
+	private static char m = '_';
+	public static void map(int[][] v) {
+		 v = new int [5][5];
+		 for (int x = 0; x < 5; x++) {
+			 for (int y = 0; y < 5; y++) {
+				 v [x][y]=m;
+				
+			 }
+			 
+		 }
+	}
+	
+
+}
